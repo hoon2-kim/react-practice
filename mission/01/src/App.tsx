d8a73c02a30c59143bb76eff7d80e1555603a913
hoon2-kim <gooaba1204@gmail.com>
2025-09-13 01:59:32 +0900

    컴포넌트 기초

---
+++ mission/01/src/App.tsx
@@ -0,0 +1,13 @@
+import Footer from './components/Footer';
+import Header from './components/header';
+import Main from './components/Main';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
